e537b139631b33f344aec3fdc2740d9aa38e2103
Saitou-C <139321510+Saitou-C@users.noreply.github.com>
2023-09-19 11:33:13 +0000

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  root to: 'public/homes#top'
+  get "about" => "public/homes#about"
+
+  resources :customers, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
