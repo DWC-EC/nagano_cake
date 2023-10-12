b20064f285bff7aae5289a8a8391a472d3756775
Saitou-C <139321510+Saitou-C@users.noreply.github.com>
2023-10-12 18:43:51 +0000

    admin/homes/top

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,6 @@ class Admin::OrdersController < ApplicationController
 end
 
 def show
+  @order = Order.find(params[:id])
+  @order_items = @order.order_item
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,7 +4,7 @@ class Public::OrdersController < ApplicationController
     @customer = current_customer
   end
 
-  def confirm #注文情報入力画面
+  def confirm #注文情報入力画面　Orderモデルに保存
     @cart_items = current_customer.cart_items
     @order = Order.new(order_params) #ストロングパラメータ
     @order.customer_id = current_customer.id
@@ -20,14 +20,14 @@ class Public::OrdersController < ApplicationController
   def complete
   end
 
-  def create#注文情報保存
+  def create#注文情報保存　OrderItemモデルに保存
     @order = Order.new(order_params) #ストロングパラメータ
-    @order.save
-    
+    @order.save #confirmを保存？
+
     #order_item(注文詳細)への保存　カートに入れた商品の種類毎にデータがあるのでeach
     current_customer.cart_items.each do |cart_item|
-      @order_item = OrderItem.new #初期化宣言←？
-      @order_item.order_id =  @order.id #注文ID←(会員ID.送料.請求金額.支払方法.配送先)
+      @order_item = OrderItem.new
+      @order_item.order_id =  @order.id #注文ID←(confirm 会員ID.送料.請求金額.支払方法.配送先)
       @order_item.item_id = cart_item.item_id #商品ID
       @order_item.amount = cart_item.amount #個数
       @order_item.price = (cart_item.item.price*1.1).floor #消費税込み価格
@@ -48,7 +48,7 @@ class Public::OrdersController < ApplicationController
 
 
   private
-  def order_params
+  def order_params #送料,支払方法,郵便番号,住所,名前,請求金額,会員ID
     params.require(:order).permit(:postage, :payment_method, :postal_code, :address, :name, :billing_amount, :customer_id)
   end
 end
